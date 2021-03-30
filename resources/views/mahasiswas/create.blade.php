5ca43dab44b341d11140a2ec9a1186d8121caf94
Babyslam <rezaldy.rafi2@gmail.com>
2021-03-30 17:43:58 +0700

    menambahkan kolom email dan tanggal lahir agar bisa diinput

--- resources/views/mahasiswas/create.blade.php
+++ resources/views/mahasiswas/create.blade.php
@@ -47,6 +47,14 @@
                     <label for="No_Handphone">No_Handphone</label> 
                     <input type="No_Handphone" name="No_Handphone" class="form-control" id="No_Handphone" aria-describedby="No_Handphone" > 
                 </div>
+                <div class="form-group">
+                    <label for="Email">Email</label>
+                    <input type="Email" name="Email" class="form-control" id="Email" aria-describedby="Email">
+                    </div>
+                <div class="form-group">
+                    <label for="Tgl_lahir">Tanggal Lahir</label>
+                    <input type="date" name="Tgl_lahir" class="form-control" id="Tgl_lahir" aria-describedby="Tgl_lahir">
+                </div>
             <button type="submit" class="btn btn-primary">Submit</button>
             </form>
             </div>
