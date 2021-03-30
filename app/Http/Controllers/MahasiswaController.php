8bcbf71d9dc4095c860ba5b8ef9fd877fc1d31c1
Babyslam <rezaldy.rafi2@gmail.com>
2021-03-30 10:47:53 +0700

    Membuat Resource Controller dan Route yang berfungsi untuk route CRUD

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
@@ -13,72 +14,79 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        //
+        //fungsi eloquent menampilkan data menggunakan pagination
+        $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel
+        $posts = Mahasiswa::orderBy('NIM', 'desc')->paginate(6);
+        return view('mahasiswas.index', compact('mahasiswas')) -> 
+        with('i', (request()->input('page', 1) - 1) * 5);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('mahasiswas.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        //melakukan validasi data
+            $request->validate([
+                'NIM' => 'required',
+                'Nama' => 'required',
+                'Kelas' => 'required',
+                'Jurusan' => 'required',
+                'No_Handphone' => 'required',
+            ]);
+        
+            //fungsi eloquent untuk menambah data
+            Mahasiswa::create($request->all());
+           
+            //jika data berhasil ditambahkan, akan kembali ke halaman utama
+            return redirect()->route('mahasiswas.index')
+                ->with('success', 'Mahasiswa Berhasil Ditambahkan');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    
+    public function show($NIM)
     {
-        //
+        //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
+            $Mahasiswa = Mahasiswa::find($NIM);
+            return view('mahasiswas.detail', compact('Mahasiswa'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    
+    public function edit($NIM)
     {
-        //
+        //menampilkan detail data dengan menemukan berdasarkan Nim Mahasiswa untuk diedit
+            $Mahasiswa = Mahasiswa::find($NIM);
+            return view('mahasiswas.edit', compact('Mahasiswa'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    
+    public function update(Request $request, $NIM)
     {
-        //
+            //melakukan validasi data
+                $request->validate([
+                    'NIM' => 'required',
+                    'Nama' => 'required',
+                    'Kelas' => 'required',
+                    'Jurusan' => 'required',
+                    'No_Handphone' => 'required',
+                ]);
+
+            //fungsi eloquent untuk mengupdate data inputan kita
+                 Mahasiswa::find($NIM)->update($request->all());
+
+            //jika data berhasil diupdate, akan kembali ke halaman utama
+                return redirect()->route('mahasiswas.index')
+                ->with('success', 'Mahasiswa Berhasil Diupdate');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    
+    public function destroy($NIM)
     {
-        //
+        //fungsi eloquent untuk menghapus data
+            Mahasiswa::find($NIM)->delete();
+            return redirect()->route('mahasiswas.index')
+                -> with('success', 'Mahasiswa Berhasil Dihapus');
     }
 }
